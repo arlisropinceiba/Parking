85b93c3df827d0af04734f27d992395c40732f3b
arlisropinceiba <arlin.ropero@ceiba.com.co>
2022-02-09 16:15:09 -0500

    Se agrega presentación

---
+++ Presentation/Presentation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Presentation
+//
+//  Created by Arlin Lisette Ropero Infante - Ceiba Software on 9/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Presentation/PresentationUITests/PresentationUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  PresentationUITestsLaunchTests.swift
+//  PresentationUITests
+//
+//  Created by Arlin Lisette Ropero Infante - Ceiba Software on 9/02/22.
+//
+
+import XCTest
+
+class PresentationUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
