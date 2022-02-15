3654f28180ed5f3ce9b3a4be31f5640b14121f58
arlisropinceiba <arlin.ropero@ceiba.com.co>
2022-02-15 11:03:53 -0500

    Ajuste en Jenkinsfile

--- Presentation/Presentation/Home/HomeView.swift
+++ Presentation/Presentation/Home/HomeView.swift
@@ -13,12 +13,7 @@ class HomeView: UIViewController {
     var presenter: HomePresenterProtocol?
     var date: Date = Date()
     
-    var clockLabel: UILabel {
-        let label = UILabel()
-        label.adjustsFontSizeToFitWidth = true
-        label.font = UIFont.systemFont(ofSize: 45)
-        return label
-    }
+    var clockLabel = UILabel()
     
     var counterLabel: UILabel {
         let label = UILabel()
@@ -34,40 +29,39 @@ class HomeView: UIViewController {
         return view
     }
     
-//    var containerView: UIView {
-//        let view = UIView()
-//        view.setTranslatesAutoresizingMaskIntoConstraints = false
-//        return view
-//    }
-    
-    var increaseHourButton: UIButton {
-        let button = UIButton()
-        button.setTitle("+1H", for: .normal)
-        return button
+    var containerView: UIView {
+        let view = UIView()
+        view.backgroundColor = .black
+        return view
     }
+    
+    var increaseHourButton = UIButton()
     
     // MARK: Lifecycle
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        clockLabel.adjustsFontSizeToFitWidth = true
+        clockLabel.numberOfLines = 0
+        clockLabel.textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        clockLabel.font = UIFont.systemFont(ofSize: 45)
+        clockLabel.text = date.inHourDateFormat()
+        clockLabel.textAlignment = .left
+        view.addSubview(clockLabel)
         
-//        let clockView = containerView
-//        view.addSubview(clockView)
-//        
-//        let viewsDictionary = ["view": clockView]
-//        
-//        clockView.addConstraints( NSLayoutConstraint.constraints(withVisualFormat: "H:|-[view]-|", options: [], metrics: nil, views: viewsDictionary))
-//        clockView.addConstraints( NSLayoutConstraint.constraints(withVisualFormat: "V:|-[view]-|", options: [], metrics: nil, views: viewsDictionary))
+        increaseHourButton.setTitle("+1H", for: .normal)
+        view.addSubview(increaseHourButton)
         
-//        clockView.addSubview(clockLabel)
-//        clockView.addSubview(increaseHourButton)
-//        
-//        let clockViewsDictionary = ["label": clockLabel, "button": increaseHourButton]
-//        clockView.addConstraints( NSLayoutConstraint.constraints(withVisualFormat: "H:|[label]-15-[button(40)]|", options: [], metrics: nil, views: clockViewsDictionary))
-//        clockView.addConstraints( NSLayoutConstraint.constraints(withVisualFormat: "V:|[label]|", options: [], metrics: nil, views: clockViewsDictionary))
-//        clockView.addConstraints( NSLayoutConstraint.constraints(withVisualFormat: "V:|[button(40)]|", options: [], metrics: nil, views: clockViewsDictionary))
-
+        let viewsDictionary = [
+            "label": clockLabel,
+            "button": increaseHourButton
+        ]
+        
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-[label]-|", options: [], metrics: nil, views: viewsDictionary as [String : Any]))
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-[button(40)]-|", options: [], metrics: nil, views: viewsDictionary as [String : Any]))
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-30-[label]-15-[button(40)]-|", options: [], metrics: nil, views: viewsDictionary as [String : Any]))
+        
         
     }
     
